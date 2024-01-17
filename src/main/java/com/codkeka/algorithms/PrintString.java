a46761917deedcd320d555f6d36df7d96b178c89
채아담 <codkeka@gmail.com>
2024-01-17 22:23:48 +0900

    solve 문자열 출력하기

---
+++ src/main/java/com/codkeka/algorithms/PrintString.java
@@ -0,0 +1,11 @@
+package com.codkeka.algorithms;
+
+import java.util.Scanner;
+
+public class PrintString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
