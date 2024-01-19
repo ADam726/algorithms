afe59b6e603f8f65436a8b42d481e2fd4405fbcd
채아담 <codkeka@gmail.com>
2024-01-19 10:26:00 +0900

    solve 특수문자 출력하기

---
+++ src/main/java/com/codkeka/algorithms/SpecialCharacter.java
@@ -0,0 +1,8 @@
+package com.codkeka.algorithms;
+
+public class SpecialCharacter {
+    //특수 문자 출력하기
+    public static void main(String[] args) {
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+    }
+}
