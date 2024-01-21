d72b6efb5f452f4a0de7a33a0c72a3b3d03a9a38
채아담 <codkeka@gmail.com>
2024-01-21 21:56:43 +0900

    solve 문자열 겹쳐쓰기

--- src/main/java/com/codkeka/algorithms/OverwriteString.java
+++ src/main/java/com/codkeka/algorithms/OverwriteString.java
@@ -8,10 +8,12 @@ public class OverwriteString {
         StringBuilder answer = new StringBuilder();
         for(int i = 0; i < s; i++) {
             answer.append(my_string.charAt(i));
+            //answer += my_string.charAt(i);
         }
         answer.append(overwrite_string);
         for( int i = s + overwrite_string.length(); i < my_string.length(); i++){
             answer.append(my_string.charAt(i));
+            //answer += my_string.charAt(i);
         }
         return answer.toString();
     }
