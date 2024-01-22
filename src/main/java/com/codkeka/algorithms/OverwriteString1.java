a0c994232070760dd2745381515d2cd44408dbfa
채아담 <codkeka@gmail.com>
2024-01-22 20:43:55 +0900

    solve 문자열 겹쳐쓰기

--- src/main/java/com/codkeka/algorithms/OverwriteString1.java
+++ src/main/java/com/codkeka/algorithms/OverwriteString1.java
@@ -2,7 +2,8 @@ package com.codkeka.algorithms;
 
 import java.util.Scanner;
 //문자열 겹쳐쓰기
-public class OverwriteString {
+//substring 사용해서 해결가능
+public class OverwriteString1 {
 
     public static String solution(String my_string, String overwrite_string, int s) {
         StringBuilder answer = new StringBuilder();
